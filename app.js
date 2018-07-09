5664ccf1a01991d138bcbc4112a4f40a8b7d95e3
Kajal <kajal.giran1993@gmail.com>
2018-07-09 16:39:36 +0530

    Intializing repository and adding basic js and html

---
+++ app.js
@@ -0,0 +1,27 @@
+var a , b;
+
+// for (a = 5; a >= 1; a--){
+//     for(b = 1; b <= a; b++){
+//         document.write('*');
+//     }
+//     document.write('<br/>')
+// }
+
+
+// for (a = 1; a <= 5; a++) {
+//     for (b = 1; b <= a; b++) {
+//         document.write('*');
+//     }
+//     document.write('<br/>')
+// }
+
+
+var rowws = 5;
+var colms = 2;
+
+for (a = 1; a <= rowws; a++) {
+    for (b = 1; b <= colms; b++) {
+        document.write('1');
+    }
+    document.write('<br/>')
+}
